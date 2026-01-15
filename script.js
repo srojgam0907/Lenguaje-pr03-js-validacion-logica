683a1bea7ef56ff46ddec392e0eb9259a1dfd522
srojgam0907 <srojgam0907@g.educaand.es>
2026-01-15 14:39:36 +0100

    JS: variables definidas

---
+++ script.js
@@ -0,0 +1,11 @@
+const formulario= document.querySelector("#formulario");
+const nombre= document.querySelector("#nombre");
+const apellidos= document.querySelector("#apellidos");
+const email= document.querySelector("#email");
+const password= document.querySelector("#password");
+const password2= document.querySelector("#password2");
+const edad= document.querySelector("#edad");
+const condicioness= document.querySelector("#condiciones");
+const mensajes= document.querySelector("#mensajes");
+
+
